1933651d3190c183f15041ca9a53a372896a007e
Pavel <kulikov-pavlik@bk.ru>
2022-04-19 18:58:00 +0300

    some changes in Container.h delete const from find

--- Container.h
+++ Container.h
@@ -1,10 +1,12 @@
 #pragma once
+#include <list>
+#include <iostream>
 template <typename TKey, typename TData>
 class Container
 {
 public:
 	virtual void add(const TKey&, const TData&) = 0;
 	virtual void remove(const TKey&) = 0;
-	virtual TData& find(const TKey&) const = 0; 
+	virtual std::list<TData*> find(const TKey&) = 0;
 	virtual ~Container() = default;
 };
