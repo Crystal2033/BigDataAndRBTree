9f8a7c6625dffd1118bb76ca026907b529ba59ae
Pavel <kulikov-pavlik@bk.ru>
2022-04-28 14:08:40 +0300

    FormatCode a bit. Added new .h with main`s funcs

--- main.cpp
+++ main.cpp
@@ -5,53 +5,7 @@
 #include <cctype>
 #include "DeliveryManager.h"
 #include "Factory.h"
-
-void print_tree_for_deliv_pair_str(const std::pair<std::string*, unsigned int>& key, Delivery* const& data, int depth = 0);
-void print_tree_for_deliv_pair_float(const std::pair<float, unsigned int>& key, Delivery* const& data, int depth = 0);
-
-int user_choice(const int left_edge, const int right_edge)
-{
-	std::string choice_str;
-	int choice_number;
-
-	choice_str.clear();
-	std::cin >> choice_str;
-	choice_number = atoi(choice_str.c_str());
-
-	if (choice_number < left_edge || choice_number > right_edge) {
-		std::cout << red << "InputValueError: You have to choose value from <" << yellow << left_edge << red << "> to <" << yellow << right_edge
-			<< red << ">." << std::endl;
-		return -1;
-	}
-	return choice_number;
-}
-
-void chooseComparator()
-{
-	std::cout << cyan << "You have to choose the comparator by field for your tree (input 1-10):" << yellow << std::endl;
-	std::cout << "1. Name" << std::endl << "2. Content" << std::endl << "3. Weight" << std::endl
-		      << "4. Price" << std::endl << "5. Delivery price" << std::endl << "6. Sender (from country)" << std::endl
-		      << "7. Departure point (by company)" << std::endl << "8. Reciever (to country)" << std::endl
-		      << "9. Destination point (for company)" << std::endl << "10. Type of transport" << std::endl << "> " << white;
-}
-void greetings() {
-	std::cout << cyan << "Hello, my name is Kulikov Pavel, FIIT, M80-211B-20." << white << std::endl;
-	std::cout << cyan << "This is a programm which can store and work with your data (" << green << "add" << cyan << " / "
-		      << yellow << "find" << cyan << " / " << red << "delete" << cyan << " / " << blue << "generate" << cyan
-		      << ")." << white << std::endl << std::endl;
-}
-
-void chooseCollection() {
-	std::cout << cyan << "At first you have to choose the type of collection (input 1 or 2): " << std::endl
-		      << yellow << "1. Red Black tree." << std::endl << "2. Queue" << white << std::endl << yellow << "> " << white;
-}
-
-void chooseOperation()
-{
-	std::cout << cyan << "You have to choose one operation, which works with your comparator key:" << std::endl
-			  << green << "1. Add data." << std::endl << yellow << "2. Find data."
-			  << red << "3. Delete data." << white << std::endl;
-}
+#include "UserFuncsAndCallbacks.h"
 
 
 int main(int argc, char* argv[])
@@ -121,7 +75,8 @@ int main(int argc, char* argv[])
 							while (true)
 							{
 								//todo find, add, remove
-
+								chooseOperation();
+								choice_number = user_choice(1, 3);
 								break;
 							}
 							delete tree;
@@ -146,10 +101,7 @@ int main(int argc, char* argv[])
 			
 		}
 		
-		
-
 	}
-
 	catch (KeyNotFoundException<std::string>& err)
 	{
 		std::cout << red << err.what() << "Key value: " << yellow << err.get_error_key() << white << std::endl;
@@ -182,30 +134,3 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
-void print_tree_for_deliv_pair_str(const std::pair<std::string*, unsigned int>& key, Delivery* const& data, int depth)
-{
-	if (depth == 0)
-	{
-		std::cout << red << "ROOT: " << white << " ";
-	}
-	else
-	{
-		std::cout << azure << "Depth: " << depth << "." << white << " " << std::endl;
-	}
-	std::cout << red << "Key: " << green << *key.first << white << " " << red << "Hash: " << pink << key.second << white << " " << std::endl;
-	std::cout << blue << "Data: " << std::endl << *data << white << std::endl;
-}
-
-void print_tree_for_deliv_pair_float(const std::pair<float, unsigned int>& key, Delivery* const& data, int depth)
-{
-	if (depth == 0)
-	{
-		std::cout << red << "ROOT: " << white << " ";
-	}
-	else
-	{
-		std::cout << azure << "Depth: " << depth << "." << white << " " << std::endl;
-	}
-	std::cout << red << "Key: " << green << key.first << white << " " << red << "Hash: " << pink <<key.second << white << " " << std::endl;
-	std::cout << blue << "Data: " << std::endl << *data << white << std::endl;
-}
